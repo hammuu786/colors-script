33601b538964e53c5da3c907ab68e5a65661c7f7
hammuu786 <100405011+hammuu786@users.noreply.github.com>
2022-10-28 13:01:15 +0530

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -6,4 +6,4 @@ sudo systemctl enable httpd
 sudo service httpd start 
 sudo wget https://www.free-css.com/assets/files/free-css-templates/download/page281/koppee.zip
 sudo unzip koppee.zip
-sudo mv coffee-shop-html-template/* /var/www/html/
+sudo mv -f coffee-shop-html-template/* /var/www/html/
